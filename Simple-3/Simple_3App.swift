79ddb421ff6d7e32fb0c6ba96c1bd058e031ace6
Terence-PDX <61093911+Terence-PDX@users.noreply.github.com>
2025-12-30 12:03:18 -0800

    Initial Commit

---
+++ Simple-3/Simple_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_3App.swift
+//  Simple-3
+//
+//  Created by TDodge47 on 2025-12-30.
+//
+
+import SwiftUI
+
+@main
+struct Simple_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
